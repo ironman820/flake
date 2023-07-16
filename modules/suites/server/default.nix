a1e4b344102bc47850e2bef458e15072169ef39e
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-07-16 14:13:13 -0500

    feat: :sparkles: Bootstrap PDNS server

---
+++ modules/suites/server/default.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, system, ... }:
+
+with lib;
+let
+  cfg = config.ironman.suites.server;
+in {
+  options.ironman.suites.server = with types; {
+    enable = mkBoolOpt false "Enable the default settings?";
+  };
+
+  config = mkIf cfg.enable {
+    ironman = {
+      firmware = enabled;
+    };
+  };
+}

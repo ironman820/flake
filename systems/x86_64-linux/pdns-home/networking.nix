063be3536982aee81e3f5b7be65f74cbbf5c403d
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-07-25 20:49:27 -0500

    Move PDNS server to new IP

--- systems/x86_64-linux/pdns-home/networking.nix
+++ systems/x86_64-linux/pdns-home/networking.nix
@@ -4,7 +4,7 @@
     defaultGateway.address = "192.168.254.1";
     dhcpcd.enable = false;
     interfaces.enp1s0.ipv4.addresses = [{
-      address = "192.168.254.9";
+      address = "192.168.254.2";
       prefixLength = 24;
     }];
     nameservers = [

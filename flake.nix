3518e781a57240a18642796bde5c2fe8231d744b
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-07-28 21:09:13 +0000

    Jul 28, 2023, 4:09 PM

--- flake.nix
+++ flake.nix
@@ -10,16 +10,17 @@
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/home-manager/release-23.05";
     };
-    nixgl.src = fetchTree {
-      type = "github";
-      owner = "guibou";
-      repo = "nixgl";
-      rev = "489d6b095ab9d289fe11af0219a9ff00fe87c7c5";
-      patches = [ ./nixgl-purity.patch ];
-    };
-    # nixgl = {
-    #   inputs.nixpkgs.follows = "nixpkgs";
+    # nixgl = fetchTree {
+    #   type = "github";
+    #   owner = "guibou";
+    #   repo = "nixgl";
+    #   rev = "489d6b095ab9d289fe11af0219a9ff00fe87c7c5";
+    #   patches = [ ./nixgl-purity.patch ];
     # };
+    nixgl = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:guibou/nixgl";
+    };
     nixos-hardware.url = "github:nixos/nixos-hardware";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     # acc5f7b - IcedTea v8 Stable

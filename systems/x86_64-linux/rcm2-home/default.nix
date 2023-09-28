6933df95e27238f1c46dd9388fd0e165f9798329
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-09-28 03:25:21 +0000

    Sep 27, 2023, 10:25 PM

--- systems/x86_64-linux/rcm2-home/default.nix
+++ systems/x86_64-linux/rcm2-home/default.nix
@@ -11,14 +11,13 @@
     ];
     ironman = {
       home.extraOptions.home = {
-        file.".config/is_personal".text = ''false'';
         shellAliases = {
           "sonars" = "sonar-scanner   -Dsonar.projectKey=RCM2   -Dsonar.sources=.   -Dsonar.host.url=https://qc.desk.niceastman.com   -Dsonar.token=sqp_4de32f09e2137f5459d22b658bf98cccfc98e533";
         };
       };
-      suites = {
-        server.enable = true;
-        servers.rcm2.enable = true;
+      suites.server = {
+        enable = true;
+        rcm2.enable = false;
       };
       virtual.guest.enable = true;
     };

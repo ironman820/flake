def95fdc77ff3b72af22122a80d3f868f801f1fe
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-09-29 09:26:33 +0000

    Sep 29, 2023, 4:26 AM

--- systems/x86_64-linux/rcm2-work/default.nix
+++ systems/x86_64-linux/rcm2-work/default.nix
@@ -1,11 +1,13 @@
 { config, inputs, lib, pkgs, ... }:
+with lib;
+with lib.ironman;
 {
   imports = [
     ./hardware.nix
     ./networking.nix
   ];
 
-  config = with lib; {
+  config = {
     environment.systemPackages = with pkgs; [
       openssl
     ];
@@ -16,9 +18,9 @@
           "sonars" = "sonar-scanner   -Dsonar.projectKey=RCM2   -Dsonar.sources=.   -Dsonar.host.url=https://qc.desk.niceastman.com   -Dsonar.token=sqp_4de32f09e2137f5459d22b658bf98cccfc98e533";
         };
       };
-      suites = {
-        server.enable = true;
-        servers.rcm2.enable = true;
+      suites.server = {
+        enable = true;
+        rcm2 = enabled;
       };
       virtual.guest.enable = true;
     };

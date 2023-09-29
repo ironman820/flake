0832ecf7a3f46ed60465c6746bff36b4911e88b3
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-09-29 16:40:19 +0000

    Sep 29, 2023, 11:40 AM

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -154,7 +154,7 @@ let
         -- Enable `lukas-reineke/indent-blankline.nvim`
         -- See `:help indent_blankline.txt`
         config = function()
-          require('indent_blankline').setup {
+          require('ibl').setup {
             char = '┊',
             show_trailing_blankline_indent = false,
           }

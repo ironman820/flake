e16e5a1993b40ed0dbaa51ff68c68b3a69b89de7
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-09-29 09:00:09 +0000

    Sep 29, 2023, 4:00 AM

--- systems/x86_64-linux/liveiso/default.nix
+++ systems/x86_64-linux/liveiso/default.nix
@@ -6,12 +6,12 @@
 
   config = {
     ironman = {
-      home.extraOptions.home.file.".config/is_personal".text = ''false'';
       nix.gc = {
         dates = "03:15";
         options = "--delete-older-than 1d";
       };
       suites.server.enable = true;
+      user.name = "nixos";
     };
     system.stateVersion = "23.05";
   };

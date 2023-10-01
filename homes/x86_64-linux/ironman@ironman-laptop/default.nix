e92132eed63cc7285bce774eef4c2d1d112319e4
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-10-01 12:38:28 -0500

    fix: Update alarm timers
    Timer try 2
    
    Split timer runners
    
    Timer try  4

--- homes/x86_64-linux/ironman@ironman-laptop/default.nix
+++ homes/x86_64-linux/ironman@ironman-laptop/default.nix
@@ -10,7 +10,12 @@ in
   };
   systemd.user = {
     services = {
-      "gotobed" = {
+      "gotobedweekdays" = {
+        Unit.Description = "myalarm Go to bed";
+        Install.WantedBy = [ "default.target" ];
+        Service.ExecStart = ''${pkgs.ironman.myalarm}/bin/myalarm.py "Go To Bed!"'';
+      };
+      "gotobedweekends" = {
         Unit.Description = "myalarm Go to bed";
         Install.WantedBy = [ "default.target" ];
         Service.ExecStart = ''${pkgs.ironman.myalarm}/bin/myalarm.py "Go To Bed!"'';
@@ -18,17 +23,21 @@ in
       "shouldhaveleft" = {
         Unit.Description = "myalarm Leave!";
         Install.WantedBy = [ "default.target" ];
-        Service.ExecStart = ''${pkgs.ironman.myalarm}/bin/myalarm.py "You should have left! GO!"'';
+        Service.ExecStart = ''${pkgs.ironman.myalarm}/bin/myalarm.py "Leave for Work"'';
       };
     };
     timers = {
-      "gotobed" = {
+      "gotobedweekdays" = {
         Install.WantedBy = [ "timers.target" ];
-        Timer.OnCalendar = "Mon..Fri 23:00:00";
+        Timer.OnCalendar = "Mon..Thu,Sun 22:00,15:00";
+      };
+      "gotobedweekends" = {
+        Install.WantedBy = [ "timers.target" ];
+        Timer.OnCalendar = "Fri,Sat 23:00,15:00";
       };
       "shouldhaveleft" = {
         Install.WantedBy = [ "timers.target" ];
-        Timer.OnCalendar = "Mon..Fri 23:00:00";
+        Timer.OnCalendar = "Mon..Fri 07:10,20:00";
       };
     };
   };

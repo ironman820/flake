621964543ba71275031c45d61dda141131f4db2f
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-12-08 13:30:20 -0600

    Update SonarQube

--- modules/nixos/servers/sonarqube/default.nix
+++ modules/nixos/servers/sonarqube/default.nix
@@ -1,12 +1,25 @@
-{ config, inputs, lib, options, pkgs, ... }:
-with lib;
-with lib.ironman;
+{ config, lib, pkgs, ... }:
 let
+  inherit (lib) mkEnableOption mkIf mkOverride;
+  inherit (lib.ironman) enabled;
+
   cfg = config.ironman.servers.sonarqube;
-in
-{
-  options.ironman.servers.sonarqube = with types; {
-    enable = mkBoolOpt false "Enable Sonarqube";
+  update-containers = pkgs.writeShellScriptBin "update-containers" ''
+    SUDO=""
+    if [[ $(id -u) -ne 0 ]]; then
+      SUDO="sudo"
+    fi
+
+      images=$($SUDO ${pkgs.docker}/bin/docker ps -a --format="{{.Image}}" | sort -u)
+
+      for image in $images
+      do
+        $SUDO ${pkgs.docker}/bin/docker pull $image
+      done
+  '';
+in {
+  options.ironman.servers.sonarqube = {
+    enable = mkEnableOption "Enable Sonarqube";
   };
 
   config = mkIf cfg.enable {
@@ -15,21 +28,20 @@ in
         authentication = mkOverride 30 ''
           local all sonarqube peer
         '';
-          # ${config.ironman.networking.network}/${config.ironman.networking.prefix} all sonarqube peer
+        # ${config.ironman.networking.network}/${config.ironman.networking.prefix} all sonarqube peer
         enable = true;
-        script = [
-          ''
-            CREATE ROLE sonarqube WITH PASSWORD 'sonarqube' CREATEDB LOGIN;
-            CREATE DATABASE sonarqube;
-            GRANT ALL PRIVILEGES ON DATABASE sonarqube TO sonarqube;
-          ''
-        ];
+        script = [''
+          CREATE ROLE sonarqube WITH PASSWORD 'sonarqube' CREATEDB LOGIN;
+          CREATE DATABASE sonarqube;
+          GRANT ALL PRIVILEGES ON DATABASE sonarqube TO sonarqube;
+        ''];
       };
       virtual.docker = enabled;
     };
-    networking.firewall.allowedTCPPorts = [
-      9000
+    environment.systemPackages = [
+      update-containers
     ];
+    networking.firewall.allowedTCPPorts = [ 9000 ];
     virtualisation.oci-containers = {
       backend = "docker";
       containers.sonarqube = {
@@ -39,13 +51,9 @@ in
           SONAR_JDBC_USERNAME = "sonarqube";
           SONAR_JDBC_PASSWORD = "sonarqube";
         };
-        extraOptions = [
-          "--network=host"
-        ];
+        extraOptions = [ "--network=host" ];
         image = "sonarqube:community";
-        ports = [
-          "9000:9000"
-        ];
+        ports = [ "9000:9000" ];
         # user = "${config.ironman.user.name}:${config.users.users.${config.ironman.user.name}.group}";
         volumes = [
           "sonarqube_data:/opt/sonarqube/data"

6ad8b78694ae5716de307ef6db9f714d727411ed
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-01-19 16:47:58 -0600

    Installed phpcbf on RCM

--- packages/php/default.nix
+++ packages/php/default.nix
@@ -1,6 +1,8 @@
 { inputs, pkgs, ... }:
 let
   inherit (inputs.nixpkgs-ba45a55.legacyPackages.${pkgs.system}.php74) buildEnv;
+  # inherit (inputs.nixpkgs-ba45a55.legacyPackages.${pkgs.system}.php74Packages)
+  # php-cs-fixer phpcbf;
 in buildEnv {
-  extensions = { all, enabled }: enabled ++ (with all; [ sqlsrv ]);
+  extensions = { all, enabled }: enabled ++ (with all; [ phpcbf sqlsrv ]);
 }

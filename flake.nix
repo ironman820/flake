c7e9079d465c14a33b7ccc6df05b1099adceded9
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-02-14 16:19:47 -0600

    Updated inputs

--- flake.nix
+++ flake.nix
@@ -7,42 +7,42 @@
     # Snowfallorg's Flake utility
     flake = {
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "https://flakehub.com/f/snowfallorg/flake/1.*.tar.gz";
+      url = "github:snowfallorg/flake";
     };
     # Home manager to keep track of dotfiles
     home-manager = {
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
     };
     # Package repo where all of my custom nix packages are stored
     ironman-apps = {
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:ironman820/ironman-apps/snowfall";
+      url = "github:ironman820/ironman-apps";
     };
     # Nix-LD is a dynamic linker that tries to mimick FHS file systems for hard-coded applications
     nix-ld = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:mic92/nix-ld";
     };
-    # Unstable repo for latest and greatest packages
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz";
-    # Nixos curated Hardware settings/drivers
-    nixos-hardware.url = "https://flakehub.com/f/NixOS/nixos-hardware/0.1.*.tar.gz";
     # Standard Nixpkgs
-    nixpkgs-2311.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2311.*.tar.gz";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    # Nixos curated Hardware settings/drivers
+    nixos-hardware.url = "github:nixos/nixos-hardware";
     # acc5f7b - IcedTea v8 Stable
     nixpkgs-acc5f7b.url = "github:nixos/nixpkgs/acc5f7b";
     # ba45a55 - The last stable update of PHP 7.4
     nixpkgs-ba45a55.url = "github:nixos/nixpkgs/ba45a55";
     snowfall-lib = {
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:snowfallorg/lib/dev";
+      url = "github:snowfallorg/lib";
     };
     # SOPS Based secret management to encrypt secrets updated to Github
     sops-nix = {
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "https://flakehub.com/f/Mic92/sops-nix/0.1.*.tar.gz";
+      url = "github:Mic92/sops-nix";
     };
+    # Unstable repo for latest and greatest packages
+    unstable.url = "github:NixOS/nixpkgs";
   };
 
   # Our config that sets up systems
@@ -80,6 +80,13 @@
 
       systems.modules = {
         nixos = with inputs; [
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+            };
+          }
           nix-ld.nixosModules.nix-ld
           sops-nix.nixosModules.sops
         ];

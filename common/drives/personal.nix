581931fc98432bb51b0a4d689c8236bc325c04a8
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-03-10 09:43:42 -0500

    Setup home shares
    Home shares on Friday
    
    Update personal shares to Nix way
    
    Removed SAMBA with autofs

--- common/drives/personal.nix
+++ common/drives/personal.nix
@@ -1,15 +1,29 @@
-{config, ...}: let
-  sopsFile = ./secrets/personal.yml;
+{
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (builtins) toString;
 in {
   mine = {
-    drives.autofs = {
-      enable = true;
-      shares = [
-        "/run/media/${config.mine.user.name}/home-nas ${config.sops.secrets.home-nas.path} --timeout 60 --browse"
-      ];
-    };
     sops.secrets = {
-      home-nas = {inherit sopsFile;};
+      home-nas = {
+        sopsFile = ./secrets/personal.yml;
+      };
     };
+  };
+  environment.systemPackages = with pkgs; [
+    cifs-utils
+    enum4linux
+    suidChroot
+  ];
+  fileSystems."/mnt/nas/media" = {
+    device = "//192.168.254.252/Media";
+    fsType = "cifs";
+    options = let
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      gid = toString config.users.groups."users".gid;
+      uid = toString config.users.users.${config.mine.user.name}.uid;
+    in ["${automount_opts},credentials=${config.sops.secrets.home-nas.path},uid=${uid},gid=${gid}"];
   };
 }
--- systems/x86_64-linux/friday/default.nix
+++ systems/x86_64-linux/friday/default.nix
@@ -4,6 +4,7 @@
   pkgs,
   ...
 }: let
+  inherit (builtins) toString;
   inherit (lib) mkForce;
 in {
   imports = [
@@ -16,16 +17,20 @@ in {
       sops.secrets.nas_auth.sopsFile = ./secrets/secrets.yml;
       suites.virtual-workstation.enable = true;
     };
-    environment.systemPackages = [
-      pkgs.cifs-utils
+    environment.systemPackages = with pkgs; [
+      cifs-utils
+      enum4linux
+      suidChroot
     ];
-    fileSystems."/run/media/${config.mine.user.name}" = {
-      device = "//192.168.254.252/volume1/Media";
+    fileSystems."/mnt/nas" = {
+      device = "//192.168.254.252/Media";
       fsType = "cifs";
       options = let
-        inherit (config.users.users.${config.mine.user.name}) uid;
-        inherit (config.users.groups."users") gid;
+        # inherit (config.users.users.${config.mine.user.name}) uid;
+        # inherit (config.users.groups."users") gid;
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        gid = toString config.users.groups."users".gid;
+        uid = toString config.users.users.${config.mine.user.name}.uid;
       in ["${automount_opts},credentials=${config.sops.secrets.nas_auth.path},uid=${uid},gid=${gid}"];
     };
     system.stateVersion = "23.05";

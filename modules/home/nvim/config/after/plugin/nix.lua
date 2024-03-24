8b225278fb66419f5032838e3529e3ae99554815
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-03-24 11:31:53 -0500

    Added snippets for more nix modules

--- modules/home/nvim/config/after/plugin/nix.lua
+++ modules/home/nvim/config/after/plugin/nix.lua
@@ -28,6 +28,39 @@ local k = require("luasnip.nodes.key_indexer").new_key
 
 ls.add_snippets("nix", {
   s(
+    "nixhome",
+    fmt(
+      [[
+        {{
+            # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+            # as well as the libraries available from your flake's inputs.
+            lib,
+            # An instance of `pkgs` with your overlays and packages applied is also available.
+            pkgs,
+            # You also have access to your flake's inputs.
+            inputs,
+
+            # Additional metadata is provided by Snowfall Lib.
+            home, # The home architecture for this host (eg. `x86_64-linux`).
+            target, # The Snowfall Lib target for this home (eg. `x86_64-home`).
+            format, # A normalized name for the home target (eg. `home`).
+            virtual, # A boolean to determine whether this home is a virtual target using nixos-generators.
+            host, # The host name for this home.
+
+            # All other arguments come from the home home.
+            config,
+            ...
+        }}:
+        {{
+          {}
+        }}
+      ]],
+      {
+        i(0),
+      }
+    )
+  ),
+  s(
     "nixmod",
     fmt(
       [[
@@ -75,12 +108,20 @@ ls.add_snippets("nix", {
     "nixoverlay",
     fmt(
       [[
-{{ channels, ... }}:
-final: prev:
-{{
-{}
-}}
-  ]],
+        # Snowfall Lib provides access to your current Nix channels and inputs.
+        #
+        # Channels are named after NixPkgs instances in your flake inputs. For example,
+        # with the input `nixpkgs` there will be a channel available at `channels.nixpkgs`.
+        # These channels are system-specific instances of NixPkgs that can be used to quickly
+        # pull packages into your overlay.
+        #
+        # All other arguments for this function are your flake inputs.
+        {{ channels, ... }}:
+
+        final: prev: {{
+            {}
+        }}
+      ]],
       {
         i(0),
       }

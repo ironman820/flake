9871f97b5457ab7ffd87015712cd43578bf0cba0
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-03-25 08:57:40 -0500

    enable inpermanence on ironman-laptop

--- systems/x86_64-linux/ironman-laptop/default.nix
+++ systems/x86_64-linux/ironman-laptop/default.nix
@@ -2,13 +2,14 @@
   inherit (lib.mine) enabled;
 in {
   imports = [
-    ../../disko.nix
-    {device = "";}
+    (../../disko.nix
+      {device = "/dev/nvme0n1";})
     ./hardware.nix
   ];
 
   config = {
     mine = {
+      inpermanence = enabled;
       suites.laptop = enabled;
     };
     system.stateVersion = "23.05";

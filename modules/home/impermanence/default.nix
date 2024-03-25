3fc595f0bdc9dbfec635806c8c5682a012aab673
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-03-25 10:27:27 -0500

    Update disk partition labels

--- modules/home/impermanence/default.nix
+++ modules/home/impermanence/default.nix
@@ -19,7 +19,7 @@ in {
   config = mkIf cfg.enable {
     boot.initrd.postDeviceCommands = lib.mkAfter ''
       mkdir /btrfs_tmp
-      mount /dev/disk/by-partlabel/nixroot /btrfs_tmp
+      mount /dev/disk/by-partlabel/disk-main-root /btrfs_tmp
       if [[ -e /btrfs_tmp/root ]]; then
           mkdir -p /btrfs_tmp/old_roots
           timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")

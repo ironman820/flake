921c02c46c9421159752d371fa18fa92cd18b78d
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-06-04 16:42:07 -0500

    Migrate the server suite

--- cells/nixos/nixosSuites.nix
+++ cells/nixos/nixosSuites.nix
@@ -2,71 +2,98 @@
   cell,
   inputs,
 }: let
-  inherit (inputs) stylix;
+  inherit (cell) nixosProfiles;
+  inherit (inputs) nixpkgs sops-nix;
+  inherit (inputs.cells) mine;
   boot = inputs.cells.boot.nixosProfiles;
+  l = nixpkgs.lib // mine.lib // builtins;
+  networking = inputs.cells.networking.nixosProfiles;
+  servers = inputs.cells.servers.nixosProfiles;
 in rec {
-  workstation = [
-    boot.grub
-    # stylix.nixosModules.stylix
-    # de.hyprland
-    #   gui-apps = {
-    #     alacritty = mkIf (terminal == "alacritty") enabled;
-    #     contour = mkIf (terminal == "contour") enabled;
-    #     floorp = mkIf (browser == "floorp") enabled;
-    #     kitty = mkIf (terminal == "kitty") enabled;
-    #     others = enabled;
-    #     wezterm = mkIf (terminal == "wezterm") enabled;
-    #     winbox = enabled;
-    #   };
-    #   hardware = {
-    #     sound = enabled;
-    #     yubikey = enabled;
-    #   };
-    #   libraries.java = enabled;
-    #   networking.basic.networkmanager = enabled;
-    #   sops = enabled;
-    #   servers.sync = enabled;
-    #   tui = {
-    #     flatpak = enabled;
-    #     neomutt = enabled;
-    #   };
-    #   virtual.host = enabled;
-    #   xdg = enabled;
-    # boot.kernel.sysctl = {
-    #   "vm.overcommit_memory" = 1;
-    # };
-    # environment.systemPackages = with pkgs; [
-    #   hplip
-    #   ntfs3g
-    #   wireguard-tools
-    # ];
-    # programs.system-config-printer = enabled;
-    # services = {
-    #   avahi = enabled;
-    #   printing = {
-    #     enable = true;
-    #     cups-pdf = enabled;
-    #     drivers = with pkgs; [gutenprint hplip];
-    #   };
-    # };
+  base = [
+    networking.dhcp
+    nixosProfiles.sops
+    servers.ssh
+    sops-nix.nixosModules.sops
   ];
-  laptop' = workstation; # ++ [
-  #   hardware = {
-  #     bluetooth = enabled;
-  #     intel-video = enabled;
-  #     power = enabled;
-  #   };
-  #   firmware = enabled;
-  #   networking.profiles = enabled;
-  #   services = {
-  #     logind = {
-  #       killUserProcesses = true;
-  #       lidSwitchExternalPower = "ignore";
-  #     };
-  #     xserver.libinput = {
-  #       enable = true;
-  #       touchpad.naturalScrolling = true;
-  #     };
-  #   };
-  # ];
+  laptop' = l.concatLists [
+    workstation
+    # [
+    #   hardware = {
+    #     bluetooth = enabled;
+    #     intel-video = enabled;
+    #     power = enabled;
+    #   };
+    #   firmware = enabled;
+    #   networking.profiles = enabled;
+    #   services = {
+    #     logind = {
+    #       killUserProcesses = true;
+    #       lidSwitchExternalPower = "ignore";
+    #     };
+    #     xserver.libinput = {
+    #       enable = true;
+    #       touchpad.naturalScrolling = true;
+    #     };
+    #   };
+    #   ]
+  ];
+  server = l.concatLists [
+    base
+    [
+      boot.systemd
+      nixosProfiles.ld-cc
+      nixosProfiles.power-performance
+      nixosProfiles.sudo-no-password
+      servers.ssh-pass-auth
+    ]
+  ];
+  workstation = l.concatLists [
+    base
+    [
+      boot.grub
+      # stylix.nixosModules.stylix
+      # de.hyprland
+      #   gui-apps = {
+      #     alacritty = mkIf (terminal == "alacritty") enabled;
+      #     contour = mkIf (terminal == "contour") enabled;
+      #     floorp = mkIf (browser == "floorp") enabled;
+      #     kitty = mkIf (terminal == "kitty") enabled;
+      #     others = enabled;
+      #     wezterm = mkIf (terminal == "wezterm") enabled;
+      #     winbox = enabled;
+      #   };
+      #   hardware = {
+      #     sound = enabled;
+      #     yubikey = enabled;
+      #   };
+      #   libraries.java = enabled;
+      #   networking.basic.networkmanager = enabled;
+      #   sops = enabled;
+      #   servers.sync = enabled;
+      #   tui = {
+      #     flatpak = enabled;
+      #     neomutt = enabled;
+      #   };
+      #   virtual.host = enabled;
+      #   xdg = enabled;
+      # boot.kernel.sysctl = {
+      #   "vm.overcommit_memory" = 1;
+      # };
+      # environment.systemPackages = with pkgs; [
+      #   hplip
+      #   ntfs3g
+      #   wireguard-tools
+      # ];
+      # programs.system-config-printer = enabled;
+      # services = {
+      #   avahi = enabled;
+      #   printing = {
+      #     enable = true;
+      #     cups-pdf = enabled;
+      #     drivers = with pkgs; [gutenprint hplip];
+      #   };
+      # };
+    ]
+  ];
 }

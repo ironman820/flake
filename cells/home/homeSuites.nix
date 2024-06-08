c44cc88e656065258427ca4a8510f7eea68bccdf
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-06-08 14:23:51 -0500

    Migrate ssh home module

--- cells/home/homeSuites.nix
+++ cells/home/homeSuites.nix
@@ -6,14 +6,26 @@
   inherit (inputs) nixpkgs sops-nix;
   inherit (inputs.cells) mine;
   l = nixpkgs.lib // mine.lib // builtins;
+  ssh = inputs.cells.ssh.homeProfiles;
 in rec {
   base = [
     homeProfiles.sops
     # hypridle.homeManagerModules.hypridle
     # hyprlock.homeManagerModules.hyprlock
     # impermanence.nixosModules.home-manager.impermanence
+    ssh.auth-keys
     sops-nix.homeManagerModules.sops
     # stylix.homeManagerModules.stylix
     {home.stateVersion = "23.05";}
+  ];
+  laptop' = l.concatLists [
+    workstation
+    []
+  ];
+  workstation = l.concatLists [
+    base
+    [
+      ssh.config
+    ]
   ];
 }

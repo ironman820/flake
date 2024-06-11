517491689e8e843211086af8d956cb172058eeb9
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-06-10 22:21:34 -0500

    Migrate sddm-catppuccin package

--- cells/mine/packages/default.nix
+++ cells/mine/packages/default.nix
@@ -29,6 +29,34 @@ in {
   networkmanagerapplet = nixpkgs.networkmanagerapplet.override {
     libnma = nixpkgs.libnma-gtk4;
   };
+  sddm-catppuccin = let
+    inherit (nixpkgs.qt5) qtquickcontrols2;
+    inherit (nixpkgs.qt6) qtbase qtsvg wrapQtAppsHook;
+  in
+    mkDerivation {
+      dontBuild = true;
+      pname = "sddm-catppuccin";
+      version = "1.0";
+
+      nativeBuildInputs = [
+        qtbase
+        wrapQtAppsHook
+      ];
+
+      propagatedUserEnvPkgs = [
+        qtbase
+        qtquickcontrols2
+        qtsvg
+      ];
+
+      src = inputs.sddm-catppuccin;
+
+      installPhase = ''
+        mkdir -p $out/share/sddm/themes/catppuccin-mocha/
+        cp $src/pertheme/mocha.conf $out/share/sddm/themes/catppuccin-mocha/theme.conf
+        cp -R $src/src/* $out/share/sddm/themes/catppuccin-mocha/
+      '';
+    };
   tochd = let
     inherit (nixpkgs) p7zip mame-tools;
     inherit (nixpkgs.python3Packages) buildPythonApplication;

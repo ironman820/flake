7f647173493d262e96bf9324ed273cfe09c0fab8
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-06-11 17:58:44 -0500

    Update personal info vars

--- cells/home/homeProfiles/vars.nix
+++ cells/home/homeProfiles/vars.nix
@@ -18,17 +18,20 @@ in {
       fileManager = l.mkOpt t.str a.fileManager "Preferred file manager";
       terminal = l.mkOpt t.str a.terminal "Preferred terminal";
     };
+    email = l.mkOpt t.str c.email "User's email";
     fonts = {
       terminalFont = l.mkOpt t.str cf.terminalFont "Default font for the terminal";
       terminalSize = l.mkOpt t.float cf.terminalSize "Size of terminal font";
       waybarSize = l.mkOpt t.int cf.waybarSize "Size of font on waybar";
     };
+    fullName = l.mkOpt t.str c.fullName "User's full name";
     transparency = {
       applicationOpacity = l.mkOpt t.float tr.applicationOpacity "Opacity of applications";
       desktopOpacity = l.mkOpt t.float tr.desktopOpacity "Opacity for desktop items";
       inactiveOpacity = l.mkOpt t.float tr.inactiveOpacity "Interactive application opacity";
       terminalOpacity = l.mkOpt t.float tr.terminalOpacity "Opacity of terminal applications";
     };
+    username = l.mkOpt l.str c.username "Username for this user";
     wallpaper = l.mkOpt (t.either t.path t.str) c.wallpaper "Wallpaper location";
   };
 }

ca0dc152eb544133a7ae79a937bec6e11de3b968
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-06-15 08:22:56 -0500

    Temporary SSH fix

--- cells/nixos/nixosProfiles/ssh-server.nix
+++ cells/nixos/nixosProfiles/ssh-server.nix
@@ -5,7 +5,9 @@
   inherit (inputs) nixpkgs;
   inherit (inputs.cells) mine;
   l = nixpkgs.lib // mine.lib // builtins;
+  # p = mine.packages;
 in {
+  # programs.ssh.package = p.openssh;
   services.openssh = {
     enable = true;
     settings = {

7b79d60e40c3c5b52766a3c384531026cbee44d3
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-06-17 10:06:01 -0500

    replace openssh with version from 23.11
    The new version does not support +ssh-dss that is needed for switches at
    work

--- cells/nixos/nixosProfiles/ssh-server.nix
+++ cells/nixos/nixosProfiles/ssh-server.nix
@@ -5,9 +5,9 @@
   inherit (inputs) nixpkgs;
   inherit (inputs.cells) mine;
   l = nixpkgs.lib // mine.lib // builtins;
-  # p = mine.packages;
+  p = mine.packages;
 in {
-  # programs.ssh.package = p.openssh;
+  programs.ssh.package = p.openssh;
   services.openssh = {
     enable = true;
     settings = {

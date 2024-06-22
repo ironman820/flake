216ae7af72c6c99f386c20a7f678eb5db045bdd4
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-06-21 21:06:57 -0500

    Update nixos-anywhere install options
    Disable automatic reboot

--- install.sh
+++ install.sh
@@ -19,12 +19,12 @@ install -d -m755 "$temp/home/$user/.config/sops/age"
 # Decrypt your private key from the password store and copy it to the temporary directory
 cp /etc/nixos/keys.txt "$temp/etc/nixos/"
 cp /etc/nixos/keys.txt "$temp/home/$user/.config/sops/age/"
-cp -r "$PWD/" "$temp/home/$user/.config/"
+# cp -r "$PWD/" "$temp/home/$user/.config/"
 
 # Set the correct permissions so sshd will accept the key
-chown -R $(whoami):users "$temp/home/$user"
+# chown -R $(whoami):users "$temp/home/$user"
 chmod 644 "$temp/etc/nixos/keys.txt"
 chmod 644 "$temp/home/$user/.config/sops/age/keys.txt"
 
 # Install NixOS to the host system with our secrets
-nix run github:nix-community/nixos-anywhere -- -i /home/$(whoami)/.ssh/deploy_ed25519 --extra-files "$temp" --flake "$flake" root@$ip
+nix run github:nix-community/nixos-anywhere -- -i /home/$(whoami)/.ssh/deploy_ed25519 --extra-files "$temp" --flake "$flake" --no-reboot root@$ip

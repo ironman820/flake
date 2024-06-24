38d95ae875ddea495f59dfe785ce4d81e68018fe
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-06-23 19:06:49 -0500

    Install auto-save plugin

---
+++ cells/mine/packages/__nixvim/plugins/autosave.nix
@@ -0,0 +1,8 @@
+{
+  plugins.auto-save = {
+    enable = true;
+    settings = {
+      enabled = true;
+    };
+  };
+}
--- cells/mine/packages/__nixvim/plugins/default.nix
+++ cells/mine/packages/__nixvim/plugins/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
     ./alpha.nix
+    ./autosave.nix
   ];
 }

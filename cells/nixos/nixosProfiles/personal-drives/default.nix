461524a783659b061623a63818da07ba3b5dbe54
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-06-29 01:03:36 -0500

    Add docker share to personal drives and update credentials

--- cells/nixos/nixosProfiles/personal-drives/default.nix
+++ cells/nixos/nixosProfiles/personal-drives/default.nix
@@ -9,20 +9,27 @@
   l = nixpkgs.lib // mine.lib // builtins;
   s = config.sops.secrets;
   v = config.vars;
+  server = "//192.168.254.252";
+  automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+  gid = l.toString config.users.groups."users".gid;
+  uid = l.toString config.users.users.${v.username}.uid;
 in {
   environment.systemPackages = with pkgs; [
     cifs-utils
     enum4linux
     suidChroot
   ];
-  fileSystems."/mnt/nas/media" = {
-    device = "//192.168.254.252/Media";
-    fsType = "cifs";
-    options = let
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      gid = l.toString config.users.groups."users".gid;
-      uid = l.toString config.users.users.${v.username}.uid;
-    in ["${automount_opts},credentials=${s.home_nas.path},uid=${uid},gid=${gid}"];
+  fileSystems = {
+    "/mnt/nas/docker" = {
+      device = "${server}/docker";
+      fsType = "cifs";
+      options = ["${automount_opts},credentials=${s.home_nas.path},uid=${uid},gid=${gid}"];
+    };
+    "/mnt/nas/media" = {
+      device = "${server}/Media";
+      fsType = "cifs";
+      options = ["${automount_opts},credentials=${s.home_nas.path},uid=${uid},gid=${gid}"];
+    };
   };
   sops.secrets.home_nas.sopsFile = ./__secrets/personal.yml;
 }

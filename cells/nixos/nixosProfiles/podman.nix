415ba370a59efb6be9b38e8dece35cf89836fc3e
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-06-29 01:04:09 -0500

    Update podman profile for cells

--- cells/nixos/nixosProfiles/podman.nix
+++ cells/nixos/nixosProfiles/podman.nix
@@ -1,27 +1,20 @@
 {
+  cell,
   config,
-  lib,
+  inputs,
   pkgs,
-  ...
 }: let
-  inherit (lib) mkEnableOption mkIf;
-  inherit (lib.mine) enabled;
-
-  cfg = config.mine.virtual.podman;
+  inherit (inputs) nixpkgs;
+  inherit (inputs.cells) mine;
+  l = nixpkgs.lib // mine.lib // builtins;
 in {
-  options.mine.virtual.podman = {
-    enable = mkEnableOption "Enable Podman";
-  };
-
-  config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      distrobox
-      podman-compose
-    ];
-    virtualisation.podman = {
-      enable = true;
-      dockerCompat = true;
-      dockerSocket = enabled;
-    };
+  environment.systemPackages = with pkgs; [
+    distrobox
+    podman-compose
+  ];
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    dockerSocket = l.enabled;
   };
 }

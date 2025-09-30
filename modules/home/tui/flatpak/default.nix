3fc75ebec89c03417678bb760eccc6a4e4997803
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-09-30 06:03:17 -0500

    Disable neomutt and update flatpak justfile

--- modules/home/tui/flatpak/default.nix
+++ modules/home/tui/flatpak/default.nix
@@ -19,7 +19,7 @@ in {
         "flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo"
         "flatpak install -uy com.usebottles.bottles"
         "flatpak install -uy com.github.tchx84.Flatseal"
-        "flatpak install -uy org.gnome.gitlab.YaLTeR.VideoTrimmer"
+        # "flatpak install -uy org.gnome.gitlab.YaLTeR.VideoTrimmer"
       ];
     };
   };

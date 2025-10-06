2e081462851f17ef92207ba36ae6c1eeb1956359
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-10-06 17:41:35 -0500

    Update settings for work laptop

--- systems/x86_64-linux/e105-laptop/hardware.nix
+++ systems/x86_64-linux/e105-laptop/hardware.nix
@@ -15,21 +15,6 @@
     extraModulePackages = [ ];
   };
 
-  fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/f0865af2-2ad5-417f-861c-ba7ba9bfe63a";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/8F03-3158";
-      fsType = "vfat";
-    };
-
-  swapDevices =
-    [{ device = "/dev/disk/by-uuid/fee5e56c-19f6-4fe7-a2c7-54337bc6178c"; }];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction

9297099033fc797e517ad551a29fd32508dc9daf
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-10-09 18:22:54 -0500

    WIP brain fried...

--- flake.nix
+++ flake.nix
@@ -4,5 +4,18 @@
     import-tree.url = "github:vic/import-tree";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake
+      {
+        inherit inputs;
+      }
+      (
+        let
+          lib = inputs.nixpkgs.lib.extend (
+            final: prev: { mine = import ./modules/_lib.nix { lib = prev; }; }
+          );
+        in
+        inputs.import-tree ./modules
+      );
 }

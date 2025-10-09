2ef6710681ff81894547dbdda3f8602b475f42e7
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-10-09 10:11:59 -0500

    WIP - restore full flake before cleanup

--- original/overlays/catppuccin/default.nix
+++
@@ -1,3 +0,0 @@
-_: final: prev: {
-  inherit (prev.mine) catppuccin-bat catppuccin-btop catppuccin-grub catppuccin-kitty catppuccin-lazygit catppuccin-neomutt catppuccin-rofi catppuccin-starship sddm-catppuccin catppuccin-yazi;
-}
--- overlays/catppuccin/default.nix
+++ overlays/catppuccin/default.nix
@@ -1,3 +1,3 @@
 _: final: prev: {
-  inherit (prev.mine) catppuccin-btop catppuccin-kitty catppuccin-lazygit catppuccin-neomutt catppuccin-rofi;
+  inherit (prev.mine) catppuccin-bat catppuccin-btop catppuccin-grub catppuccin-kitty catppuccin-lazygit catppuccin-neomutt catppuccin-rofi catppuccin-starship sddm-catppuccin catppuccin-yazi;
 }

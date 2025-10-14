5c0edd242ccb00dea67aa221878f751668e289cd
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-10-13 20:31:14 -0500

    TLP skip wifi on friday

--- systems/x86_64-linux/friday/default.nix
+++ systems/x86_64-linux/friday/default.nix
@@ -1,10 +1,8 @@
 {
-  # config,
   lib,
   pkgs,
   ...
 }: let
-  # inherit (builtins) toString;
   inherit (lib.mine) enabled;
 in {
   imports = [
@@ -16,22 +14,13 @@ in {
       cifs-utils
       enum4linux
       mmex
-      # suidChroot
     ];
     mine = {
       networking.profiles.work = true;
       sops.secrets.nas_auth.sopsFile = ./secrets/secrets.yml;
       suites.laptop = enabled;
     };
-    # fileSystems."/mnt/nas" = {
-    #   device = "//192.168.254.5/Media";
-    #   fsType = "cifs";
-    #   options = let
-    #     automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    #     gid = toString config.users.groups."users".gid;
-    #     uid = toString config.users.users.${config.mine.user.name}.uid;
-    #   in ["${automount_opts},credentials=${config.sops.secrets.nas_auth.path},uid=${uid},gid=${gid}"];
-    # };
+    services.tlp.settings.RUNTIME_PM_DENYLIST = "03:00.0";
     system.stateVersion = "25.05";
   };
 }

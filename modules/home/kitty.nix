829e8b96562c5647b1d3f4a7131315912ca85244
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-10-25 01:21:17 -0500

    Allow kitty close with process running

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -16,6 +16,7 @@
       settings = {
         bold_font = "auto";
         bold_italic_font = "auto";
+        confirm_os_window_close = 0;
         cursor_shape = "beam";
         enable_audio_bell = false;
         italic_font = "auto";

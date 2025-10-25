8a4320c9e4d911d00573b9eb5c1914231f1968e1
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-10-24 21:11:16 -0500

    basic config

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -1,10 +1,39 @@
-{ config, ...}: {
-  flake.homeModules.hyprland =
-  {
+{ config, ... }:
+{
+  flake.homeModules.hyprland = {
     imports = with config.flake.homeModules; [
       kitty
     ];
     home.sessionVariables.NIXOS_OZONE_WL = "1";
-    wayland.windowManager.hyprland.enable = true;
+    programs.bash = {
+      historyControl = [ "ignoreboth" ];
+      historySize = 32768;
+    };
+    wayland.windowManager.hyprland = {
+      enable = true;
+      settings = {
+        "$mod" = "SUPER";
+        bind = [
+          "$mod, q, exec, kitty"
+          # ", Print, exec, grimblast copy area"
+        ]
+        ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+          builtins.concatLists (
+            builtins.genList (
+              i:
+              let
+                ws = i + 1;
+              in
+              [
+                "$mod, code:1${toString i}, workspace, ${toString ws}"
+                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+              ]
+            ) 9
+          )
+        );
+      };
+    };
   };
 }

b9a77c956f91d07b8cbb3957d2aa2bde8549d758
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-10-27 22:22:58 -0500

    Walker cachix

--- modules/default-system.nix
+++ modules/default-system.nix
@@ -102,6 +102,14 @@
             "nix-command"
             "flakes"
           ];
+          extra-substituters = [
+            "https://walker.cachix.org"
+            "https://walker-git.cachix.org"
+          ];
+          extra-trusted-public-keys = [
+            "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
+            "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
+          ];
           trusted-users = [
             "root"
             "@wheel"

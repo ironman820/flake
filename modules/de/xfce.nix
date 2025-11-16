031827bb642462b2837d17c0bce10bc305229276
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-11-15 20:32:16 -0600

    switch friday to xfce

--- modules/de/xfce.nix
+++ modules/de/xfce.nix
@@ -1,14 +1,32 @@
 {
   flake.nixosModules.de-xfce =
-    { config, pkgs, ... }:
+    { pkgs, ... }:
     {
-      # environment.systemPackages = with pkgs; [
-      #   chicago95
-      # ];
-      services.xserver = {
-        enable = true;
-        desktopManager.xfce.enable = true;
-        displayManager.lightdm.enable = true;
+      boot.plymouth = {
+        theme = "Chicago95";
+        themePackages = [
+          pkgs.local.chicago95
+        ];
+      };
+      environment.systemPackages = with pkgs; [
+        local.chicago95
+        xfce.xfce4-whiskermenu-plugin
+      ];
+      services = {
+        displayManager.defaultSession = "xfce";
+        xserver = {
+          enable = true;
+          desktopManager.xfce.enable = true;
+          displayManager = {
+            lightdm = {
+              enable = true;
+              greeters.gtk.theme = {
+                name = "Chicago95";
+                package = pkgs.local.chicago95;
+              };
+            };
+          };
+        };
       };
       xdg.portal.extraPortals = [
         pkgs.xdg-desktop-portal-gtk

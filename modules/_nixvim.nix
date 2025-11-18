49d896a772face967ca6d0f10afec054e1f796cf
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-11-18 17:27:55 -0600

    nixvim-indent-blankline

--- modules/_nixvim.nix
+++ modules/_nixvim.nix
@@ -868,6 +868,31 @@
         };
       };
       colorful-menu.enable = true;
+      indent-blankline = {
+        enable = true;
+        settings = {
+          indent = {
+            char = "│";
+            tab_char = "│";
+          };
+          scope = {
+            enabled = true;
+          };
+          exclude.filetypes = [
+            "help"
+            "alpha"
+            "dashboard"
+            "neo-tree"
+            "Trouble"
+            "trouble"
+            "lazy"
+            "mason"
+            "notify"
+            "toggleterm"
+            "lazyterm"
+          ];
+        };
+      };
       luasnip = {
         enable = true;
         fromVscode = [

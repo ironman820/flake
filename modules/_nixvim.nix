acf5a9fe52a645a0062fed11489e7f325c8bc6b3
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-11-19 12:23:05 -0600

    nixvim-lualine

--- modules/_nixvim.nix
+++ modules/_nixvim.nix
@@ -979,6 +979,162 @@
           ];
         };
       };
+      lualine = {
+        enable = true;
+        luaConfig.pre = ''
+          local icons = {
+              misc = {
+                dots = "󰇘",
+              },
+              dap = {
+                Stopped             = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
+                Breakpoint          = " ",
+                BreakpointCondition = " ",
+                BreakpointRejected  = { " ", "DiagnosticError" },
+                LogPoint            = ".>",
+              },
+              diagnostics = {
+                Error = " ",
+                Warn  = " ",
+                Hint  = " ",
+                Info  = " ",
+              },
+              git = {
+                added    = " ",
+                modified = " ",
+                removed  = " ",
+              },
+              kinds = {
+                Array         = " ",
+                Boolean       = "󰨙 ",
+                Class         = " ",
+                Codeium       = "󰘦 ",
+                Color         = " ",
+                Control       = " ",
+                Collapsed     = " ",
+                Constant      = "󰏿 ",
+                Constructor   = " ",
+                Copilot       = " ",
+                Enum          = " ",
+                EnumMember    = " ",
+                Event         = " ",
+                Field         = " ",
+                File          = " ",
+                Folder        = " ",
+                Function      = "󰊕 ",
+                Interface     = " ",
+                Key           = " ",
+                Keyword       = " ",
+                Method        = "󰊕 ",
+                Module        = " ",
+                Namespace     = "󰦮 ",
+                Null          = " ",
+                Number        = "󰎠 ",
+                Object        = " ",
+                Operator      = " ",
+                Package       = " ",
+                Property      = " ",
+                Reference     = " ",
+                Snippet       = " ",
+                String        = " ",
+                Struct        = "󰆼 ",
+                TabNine       = "󰏚 ",
+                Text          = " ",
+                TypeParameter = " ",
+                Unit          = " ",
+                Value         = " ",
+                Variable      = "󰀫 ",
+              },
+            }
+            local function find_root()
+              -- Use the current buffer's path as the starting point for the git search
+              local current_file = vim.api.nvim_buf_get_name(0)
+              local current_dir
+              local cwd = vim.fn.getcwd()
+              -- If the buffer is not associated with a file, return nil
+              if current_file == "" then
+                current_dir = cwd
+              else
+                -- Extract the directory from the current file's path
+                current_dir = vim.fn.fnamemodify(current_file, ":h")
+              end
+
+              -- Find the Git root directory from the current file's path
+              local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
+              if vim.v.shell_error ~= 0 then
+                print("Not a git repository. Searching on current working directory")
+                return cwd
+              end
+              return git_root
+            end
+        '';
+        settings = {
+          options = {
+            icons_enabled = false;
+            theme = "tokyonight-night";
+            component_separators = "|";
+            section_separators = "";
+          };
+          sections = {
+            lualine_a = [ "mode" ];
+            lualine_b = [ "branch" ];
+            lualine_c.__raw = ''
+              {
+                find_root(),
+                {
+                  "diagnostics",
+                  symbols = {
+                    error = icons.diagnostics.Error,
+                    warn = icons.diagnostics.Warn,
+                    info = icons.diagnostics.Info,
+                    hint = icons.diagnostics.Hint,
+                  },
+                },
+                { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+                {
+                  'filename', path = 1, status = true,
+                },
+              }
+            '';
+            lualine_x.__raw = ''
+              {
+                {
+                  "diff",
+                  symbols = {
+                    added = icons.git.added,
+                    modified = icons.git.modified,
+                    removed = icons.git.removed,
+                  },
+                  source = function()
+                    ---@diagnostic disable-next-line
+                    local gitsigns = vim.b.gitsigns_status_dict
+                    if gitsigns then
+                      return {
+                        added = gitsigns.added,
+                        modified = gitsigns.changed,
+                        removed = gitsigns.removed,
+                      }
+                    end
+                  end,
+                },
+              }
+            '';
+            lualine_y.__raw = ''
+              {
+                { "progress", separator = " ", padding = { left = 1, right = 0 } },
+                { "location", padding = { left = 0, right = 1 } },
+              }
+            '';
+            lualine_z.__raw = ''
+              {
+                function()
+                  return " " .. os.date("%R")
+                end,
+              }
+            '';
+          };
+        };
+      };
       luasnip = {
         enable = true;
         fromVscode = [

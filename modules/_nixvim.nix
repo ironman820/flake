0391eb0dda4251b0cb9ef92e466c4dfefd7e530f
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-11-19 11:09:09 -0600

    nixvim-dropbar

--- modules/_nixvim.nix
+++ modules/_nixvim.nix
@@ -677,6 +677,24 @@
         '';
         options.desc = "Toggle Zoom";
       }
+      {
+        mode = "n";
+        key = "<Leader>;";
+        action.__raw = "require('dropbar.api').pick";
+        options.desc = "Pick symbols in winbar";
+      }
+      {
+        mode = "n";
+        key = "[;";
+        action.__raw = "require('dropbar.api').goto_context_start";
+        options.desc = "Go to start of current context";
+      }
+      {
+        mode = "n";
+        key = "];";
+        action.__raw = "require('dropbar.api').select_next_context";
+        options.desc = "Select next context";
+      }
     ];
     opts = {
       autowrite = true;
@@ -926,6 +944,7 @@
         };
       };
       colorful-menu.enable = true;
+      dropbar.enable = true;
       indent-blankline = {
         enable = true;
         settings = {

8d5bc1d3bbf8b5d543f9d631adbaca25ae901021
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-12-05 17:06:25 -0600

    update psalm package for rcm coding

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -1,11 +1,14 @@
-{
-  flake.nixosModules.nixvim = {
+{ inputs, ... }: {
+  flake.nixosModules.nixvim =
+  { pkgs, ... }:
+  {
     imports = [
       ./_nixvim.nix
     ];
     programs.nixvim = {
       enable = true;
       defaultEditor = true;
+      plugins.lsp.servers.psalm.package = inputs.nixpkgs-php.legacyPackages.${pkgs.stdenv.hostPlatform.system}.php74Packages.psalm;
     };
   };
 }

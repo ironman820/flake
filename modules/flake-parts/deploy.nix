ddd5d45974611eaa5dc4b5ae6fb8eb369e1ee6dd
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2026-01-19 18:17:52 -0600

    delete icewarp server from deploy

--- modules/flake-parts/deploy.nix
+++ modules/flake-parts/deploy.nix
@@ -1,15 +1,6 @@
 { inputs, self, ... }:
 {
   flake.deploy.nodes = {
-    icewarp = {
-      hostname = "icewarp";
-      profiles.system = {
-        user = "root";
-        path = inputs.deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.icewarp;
-      };
-      remoteBuild = true;
-      sshUser = "royell";
-    };
     llama = {
       hostname = "llama";
       profiles.system = {

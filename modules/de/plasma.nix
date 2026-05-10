b1dc9a06d159e4a1f745a69e006b8aa638fd679c
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2026-05-09 22:04:36 -0500

    replace SDDM with Plasma Login Manager

--- modules/de/plasma.nix
+++ modules/de/plasma.nix
@@ -1,14 +1,14 @@
-{ config, ... }:
+{ ... }:
 {
   flake.nixosModules.de-plasma =
   { pkgs, ... }:
   {
-    imports = with config.flake.nixosModules; [
-      sddm
-    ];
     environment.systemPackages = with pkgs.kdePackages; [
       partitionmanager
     ];
-    services.desktopManager.plasma6.enable = true;
+    services = {
+      desktopManager.plasma6.enable = true;
+      displayManager.plasma-login-manager.enable = true;
+    };
   };
 }

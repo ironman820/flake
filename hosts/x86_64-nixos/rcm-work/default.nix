5b8165d2dea0b256d3857977ee2e74a2911471f1
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2026-08-01 17:35:37 -0500

    Add sonarqube setting to RCM servers

--- hosts/x86_64-nixos/rcm-work/default.nix
+++ hosts/x86_64-nixos/rcm-work/default.nix
@@ -1,4 +1,7 @@
 {
+  config,
+  flakeRoot,
+  pkgs,
   self,
   ...
 }:
@@ -15,14 +18,26 @@
     tmux
     x64-linux
   ]);
+  environment.systemPackages = with pkgs; [
+    sonar-scanner-cli
+  ];
   home-manager = {
     users.ironman = self.homeConfigurations.ironman-server;
   };
+  networking.nameservers = [
+    "192.168.20.2"
+  ];
   nix.settings.cores = 1;
   security.sudo.wheelNeedsPassword = false;
   services = {
     openssh.settings.PermitRootLogin = "no";
     qemuGuest.enable = true;
+  };
+  sops.secrets."sonar-project.properties" = {
+    sopsFile = "${flakeRoot}/.secrets/rcm.yaml";
+    owner = config.ironman.user.name;
+    group = config.ironman.user.name;
+    path = "/data/rcm/sonar-project.properties";
   };
   users.users.ironman.extraGroups = [
     "networkmanager"

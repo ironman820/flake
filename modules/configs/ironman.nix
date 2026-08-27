9ab557ea2b4e00b644248b5ca7be118775c58a8b
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2026-08-26 22:29:56 -0500

    correct ironman module

--- modules/configs/ironman.nix
+++ modules/configs/ironman.nix
@@ -3,36 +3,35 @@
   ...
 }:
 {
-  flake = moduleWithSystem (
-    perSystem@{ inputs', ... }:
-    {
-      nixosModules.ironman =
-        { lib, pkgs, ... }:
-        let
-          inherit (lib) mkOption;
-          inherit (lib.types) bool package;
-        in
-        {
-          options.ironman = {
-            browser = mkOption {
-              default = inputs'.zen-browser.packages.default;
-              description = "Default browser to open with launchers";
-              type = package;
-            };
-            laptop = mkOption {
-              default = false;
-              description = "Is this system a laptop?";
-              type = bool;
-            };
-            terminal = mkOption {
-              default = pkgs.ghostty;
-              description = "Default terminal emulator to open with launchers";
-              type = package;
-            };
+  flake = {
+    nixosModules.ironman = moduleWithSystem (
+      perSystem@{ inputs', ... }:
+      { lib, pkgs, ... }:
+      let
+        inherit (lib) mkOption;
+        inherit (lib.types) bool package;
+      in
+      {
+        options.ironman = {
+          browser = mkOption {
+            default = inputs'.zen-browser.packages.default;
+            description = "Default browser to open with launchers";
+            type = package;
+          };
+          laptop = mkOption {
+            default = false;
+            description = "Is this system a laptop?";
+            type = bool;
+          };
+          terminal = mkOption {
+            default = pkgs.ghostty;
+            description = "Default terminal emulator to open with launchers";
+            type = package;
           };
         };
-      homeModules.ironman = {
-      };
-    }
-  );
+      }
+    );
+    homeModules.ironman = {
+    };
+  };
 }

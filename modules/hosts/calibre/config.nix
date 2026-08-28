7ce5e85f86d8cc8b3280a739a94f304c8756d81b
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2026-08-28 06:28:53 -0500

    Revert "Move calibre's networking to nix"
    This reverts commit cc79182e634a035557599978d88c3bccb8247789.

--- modules/hosts/calibre/config.nix
+++ modules/hosts/calibre/config.nix
@@ -7,20 +7,7 @@
     { config, ... }:
     {
       hardware.facter.reportPath = ./facter.json;
-      networking = {
-        defaultGateway.address = "192.168.248.1";
-        hostName = "calibre";
-        interfaces.eth0.ipv4.addresses = [
-          {
-            address = "192.168.248.101";
-            prefixLength = 23;
-          }
-        ];
-        nameservers = [
-          "192.168.248.2"
-        ];
-      };
-      proxmoxLXC.manageNetwork = true;
+      networking.hostName = "calibre";
       services = {
         calibre-web = {
           enable = true;

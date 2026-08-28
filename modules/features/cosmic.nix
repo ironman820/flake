25fd8ec61a449ffb5e150062c7ca02f4f701ea5b
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2026-08-28 13:16:31 -0500

    Cosmic autotile

--- modules/features/cosmic.nix
+++ modules/features/cosmic.nix
@@ -30,6 +30,7 @@
           in
           {
             enable = true;
+            compositor.autotile = true;
             resetFiles = true;
             resetFilesDirectories = [
               "config"

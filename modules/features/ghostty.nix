84a596ea481589b8e1f4d1187f5ba5914dc2d9ea
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2026-09-01 05:40:20 -0500

    Update ghostty transparency

--- modules/features/ghostty.nix
+++ modules/features/ghostty.nix
@@ -23,7 +23,7 @@
         enable = true;
         enableBashIntegration = true;
         settings = {
-          background-opacity = 0.80;
+          background-opacity = 0.90;
           background-blur = true;
           font-family = "IosevkaTerm Nerd Font Mono";
           font-size = 12;

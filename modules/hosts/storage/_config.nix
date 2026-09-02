5f1b91ae1c6e658518bb59bfd49c7ac8e57c999c
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2026-09-02 00:05:15 -0500

    Samba setup

--- modules/hosts/storage/_config.nix
+++ modules/hosts/storage/_config.nix
@@ -1,4 +1,4 @@
-{
+{ config, ... }: {
   hardware.facter.reportPath = ./facter.json;
   networking = {
     hostName = "storage";
@@ -6,4 +6,53 @@
       "192.168.248.2"
     ];
   };
+  services = {
+    samba = {
+      enable = true;
+      settings = {
+        global = {
+          "guest account" = "nobody";
+          "hosts allow" = "192.168. 127.0.0.1 localhost";
+          "hosts deny" = "0.0.0.0/0";
+          "map to guest" = "bad user";
+          security = "user";
+          "usershare allow guests" = "yes";
+          workgroup = "WORKGROUP";
+        };
+        data = {
+          path = "/shares/data";
+          browsable = true;
+          "create mask" = "0644";
+          "directory mask" = "0755";
+          "force user" = config.ironman.user.name;
+          "force group" = config.ironman.user.name;
+          "guest ok" = "yes";
+          "read only" = "no";
+        };
+      };
+    };
+    samba-wsdd.enable = true;
+  };
+  topology =
+    let
+      inherit (config.lib.topology) mkConnection;
+    in
+    {
+      id = "storage";
+      self = {
+        interfaces.eth0 = {
+          addresses = [
+            "192.168.248.100"
+          ];
+          network = "home";
+          physicalConnections = [
+            (mkConnection "pve" "vmbr0")
+          ];
+        };
+        deviceType = "nixos";
+        guestType = "nixos-container";
+        name = "Storage";
+        parent = "pve";
+      };
+    };
 }

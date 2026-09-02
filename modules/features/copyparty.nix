bea04ba3398e71024bd8f6fb54da4846ca60c29f
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2026-09-01 22:57:48 -0500

    Setup copyparty server config

--- modules/features/copyparty.nix
+++ modules/features/copyparty.nix
@@ -8,11 +8,41 @@
   flake = {
     nixosModules.copyparty = moduleWithSystem (
       perSystem@{ ... }:
-      _: {
+      { config, ... }: {
         imports = [
           inputs.copyparty.nixosModules.default
         ];
-        services.copyparty.enable = true;
+        services.copyparty = {
+          enable = true;
+          user = config.ironman.user.name;
+          group = config.ironman.user.name;
+          settings = {
+            i = "0.0.0.0";
+            z = true;
+          };
+          accounts.ironman.passwordFile = config.sops.secrets.ironman_password.path;
+          volumes = {
+            "/" = {
+              path = "/shares";
+              access = {
+                r = "*";
+                "rw." = [
+                  "ironman"
+                ];
+              };
+              flags = {
+                fk = 4;
+                scan = 60;
+                e2d = true;
+              };
+            };
+          };
+        };
+        sops.secrets.ironman_password = {
+          sopsFile = "${self.outPath}/.secrets/ironman.yaml";
+          owner = config.ironman.user.name;
+          group = config.ironman.user.name;
+        };
       }
     );
     homeModules.copyparty = moduleWithSystem (

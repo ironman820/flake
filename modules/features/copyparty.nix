6d6afddfa9717b67c2e7d76662798529c54d7102
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2026-09-01 23:57:04 -0500

    cleanup copyparty module

--- modules/features/copyparty.nix
+++ modules/features/copyparty.nix
@@ -1,54 +1,43 @@
 {
   inputs,
-  moduleWithSystem,
   self,
   ...
 }:
 {
-  flake = {
-    nixosModules.copyparty = moduleWithSystem (
-      perSystem@{ ... }:
-      { config, ... }: {
-        imports = [
-          inputs.copyparty.nixosModules.default
-        ];
-        services.copyparty = {
-          enable = true;
-          user = config.ironman.user.name;
-          group = config.ironman.user.name;
-          settings = {
-            i = "0.0.0.0";
-            z = true;
+  flake.nixosModules.copyparty = { config, ... }: {
+    imports = [
+      inputs.copyparty.nixosModules.default
+    ];
+    services.copyparty = {
+      enable = true;
+      user = config.ironman.user.name;
+      group = config.ironman.user.name;
+      settings = {
+        i = "0.0.0.0";
+        z = true;
+      };
+      accounts.ironman.passwordFile = config.sops.secrets.ironman_password.path;
+      volumes = {
+        "/" = {
+          path = "/shares";
+          access = {
+            r = "*";
+            "A" = [
+              "ironman"
+            ];
           };
-          accounts.ironman.passwordFile = config.sops.secrets.ironman_password.path;
-          volumes = {
-            "/" = {
-              path = "/shares";
-              access = {
-                r = "*";
-                "rw." = [
-                  "ironman"
-                ];
-              };
-              flags = {
-                fk = 4;
-                scan = 60;
-                e2d = true;
-              };
-            };
+          flags = {
+            fk = 4;
+            scan = 60;
+            e2d = true;
           };
         };
-        sops.secrets.ironman_password = {
-          sopsFile = "${self.outPath}/.secrets/ironman.yaml";
-          owner = config.ironman.user.name;
-          group = config.ironman.user.name;
-        };
-      }
-    );
-    homeModules.copyparty = moduleWithSystem (
-      perSystem@{ ... }:
-      _: {
-      }
-    );
+      };
+    };
+    sops.secrets.ironman_password = {
+      sopsFile = "${self.outPath}/.secrets/ironman.yaml";
+      owner = config.ironman.user.name;
+      group = config.ironman.user.name;
+    };
   };
 }

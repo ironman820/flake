9d328fc1724e0a9fade84950f4dce2e3463f5b52
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2026-09-06 20:40:16 -0500

    Setup soulseek server

---
+++ modules/hosts/soulseek/_config.nix
@@ -0,0 +1,32 @@
+{ config, ... }: {
+  hardware.facter.reportPath = ./facter.json;
+  ironman.slsk.enable = true;
+  networking = {
+    hostName = "soulseek";
+    nameservers = [
+      "192.168.248.2"
+    ];
+  };
+  topology =
+    let
+      inherit (config.lib.topology) mkConnection;
+    in
+    {
+      id = "soulseek";
+      self = {
+        interfaces.eth0 = {
+          addresses = [
+            "192.168.248.10"
+          ];
+          network = "home";
+          physicalConnections = [
+            (mkConnection "pve" "vmbr0")
+          ];
+        };
+        deviceType = "nixos";
+        guestType = "nixos-container";
+        name = "SoulSeek";
+        parent = "pve";
+      };
+    };
+}
---
+++ modules/hosts/soulseek/default.nix
@@ -0,0 +1,16 @@
+{
+  inputs,
+  self,
+  ...
+}:
+{
+  flake.nixosConfigurations.soulseek = inputs.nixpkgs.lib.nixosSystem {
+    modules = with self.nixosModules; [
+      ./_config.nix
+      proxmox
+      server
+      slskd
+      traefik
+    ];
+  };
+}

ee6b026a178f7c4e723e3b85c748360dd3606674
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2026-09-21 08:08:27 -0500

    convert config to module

--- modules/hosts/vaultwarden/default.nix
+++ modules/hosts/vaultwarden/default.nix
@@ -6,9 +6,9 @@
 {
   flake.nixosConfigurations.vaultwarden = inputs.nixpkgs.lib.nixosSystem {
     modules = with self.nixosModules; [
-      ./_config.nix
       proxmox
       server
+      vaultwardenConfig
     ];
   };
 }
